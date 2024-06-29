7254a27d083edd414db66b52d20cc430cf5480db
Sarawin Buakaew <sarawinbuakaewpp@gmail.com>
2024-06-29 14:24:25 +0700

    fetch api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    fetch()
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(data => {
         console.log(data)
