fc76ed22a44ce38d669f840877ed2e82dfb9b85e
Sarawin Buakaew <sarawinbuakaewpp@gmail.com>
2024-07-06 20:16:12 +0700

    edit code

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        display(data)
-     });
-
-});
